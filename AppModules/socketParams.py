eddc13cba89785e8d32b36a3c239c751279a025b
awssso-amaseghe <amaseghe@datafiedusa.com>
2023-09-04 21:32:32 +1000

    Updated params

--- AppModules/socketParams.py
+++ AppModules/socketParams.py
@@ -1,3 +1,3 @@
 # Socket parameters
 SERVER_HOST = '192.168.1.99' # Server's IP address..Can be resolved using gethostbyname() if using DNS.
-PORT = 55551   # Arbitrary non-privileged port
+PORT = 55552   # Arbitrary non-privileged port
